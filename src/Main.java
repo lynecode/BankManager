60387b66e5ec86d78f2e719513580681e95e82a4
bi tia <tiajoce7@gmail.com>
2026-05-01 15:27:36 +0000

    ajout main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        CheckingAccount checking = new CheckingAccount();
+        checking.account = "CC-1";
+        checking.balance = 2000.00;
+        checking.limit = 500.00;
+
+        System.out.println("=== Compte Courant ===");
+        System.out.println("  - Numéro  : " + checking.account);
+        System.out.println("  - Solde   : " + checking.balance + " FCFA");
+        System.out.println("  - Limite  : " + checking.limit + " FCFA");
+
+        SavingsAccount savings = new SavingsAccount();
+        savings.account = "SA-2";
+        savings.balance = 5000.00;
+        savings.interestRate = 3.5;
+
+        System.out.println("=== Compte Épargne ===");
+        System.out.println("  - Numéro  : " + savings.account);
+        System.out.println("  - Solde   : " + savings.balance + " FCFA");
+        System.out.println("  - Taux    : " + savings.interestRate + " %");
+
+        COD cod = new COD();
+        cod.account = "COD-3";
+        cod.balance = 10000.00;
+        cod.durationMonths = 12;
+
+        System.out.println("=== Certificat de Dépôt ===");
+        System.out.println("  - Numéro  : " + cod.account);
+        System.out.println("  - Solde   : " + cod.balance + " FCFA");
+        System.out.println("  - Durée   : " + cod.durationMonths + " mois");
     }
 }
